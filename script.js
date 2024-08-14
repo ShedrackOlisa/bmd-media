7b99a0e45f1e27b7f93c4efbb5a9aef4e5850be1
ShedrackOlisa <143615341+ShedrackOlisa@users.noreply.github.com>
2024-08-14 11:39:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle menu visibility on mobile
+document.querySelector('.menu-icon').addEventListener('click', function() {
+  document.querySelector('nav ul.menu').classList.toggle('show');
+});
